57a6f6204f72e2afc116721b2b86451fa19a32a6
James Morse <james.morse@arm.com>
2019-03-12 18:13:54 +0000

    x86/resctrl: Pass in the code/data/both configuration value when parsing
    The illusion of three types of cache at each level is a neat trick to
    allow a static table of resources to be used. This is a problem if the
    cache topology and partitioning abilities have to be discovered at boot.
    
    We want to fold the three code/data/both caches into one, and move the
    CDP configuration details to be a property of the configuration and
    its closid, not the cache. The resctrl filesystem can then re-create
    the illusion of separate caches.
    
    Temporarily label the configuration property of the cache, and pass
    this value down to the configuration helpers. Eventually we will move
    this label up to become a property of the schema.
    
    A later patch will correct the closid for CDP when the configuration is
    staged, which will let us merge the three types of resource.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- arch/x86/kernel/cpu/resctrl/internal.h
+++ arch/x86/kernel/cpu/resctrl/internal.h
@@ -348,6 +348,7 @@ static inline bool is_mbm_event(int e)
 
 struct rdt_parse_data {
 	struct rdtgroup		*rdtgrp;
+	enum resctrl_conf_type	conf_type;
 	char			*buf;
 };
 
@@ -361,6 +362,7 @@ struct rdt_parse_data {
 struct rdt_hw_resource {
 	struct rdt_resource     resctrl;
 	u32			hw_num_closid;
+	enum resctrl_conf_type	conf_type;	// temporary
 	unsigned int		msr_base;
 	void (*msr_update)	(struct rdt_domain *d, struct msr_param *m,
 				 struct rdt_resource *r);
