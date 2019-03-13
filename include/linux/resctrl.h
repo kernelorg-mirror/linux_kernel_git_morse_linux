abeba3eb683ccfff2e40285f66a87f8a44044f40
James Morse <james.morse@arm.com>
2019-03-13 14:18:08 +0000

    x86/resctrl: Walk the resctrl schema list instead of an arch list
    Now that resctrl has a list of resources it is using, walk that list
    instead of the architectures list. This will let us keep schema
    properties with the resource that is using them.
    
    Most users of for_each_alloc_enabled_rdt_resource() are per-schema,
    and also want a schema property, like the conf_type. Switch these to
    walk the schema list. We only created schema for alloc_eanbled
    resources.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- include/linux/resctrl.h
+++ include/linux/resctrl.h
@@ -111,6 +111,7 @@ struct resctrl_membw {
 };
 
 struct rdt_parse_data;
+struct resctrl_schema;
 
 /**
  * @rid:		The index of the resource
@@ -164,7 +165,7 @@ struct rdt_resource {
 	u32			default_ctrl;
 	const char		*format_str;
 	int			(*parse_ctrlval)(struct rdt_parse_data *data,
-						 struct rdt_resource *r,
+						 struct resctrl_schema *s,
 						 struct rdt_domain *d);
 	struct list_head	evt_list;
 	unsigned long		fflags;
@@ -179,7 +180,7 @@ int resctrl_arch_set_cdp_enabled(bool enable);
 
 /**
  * @list:	Member of resctrl's schema list
- * @cdp_type:	Whether this entry is for code/data/both
+ * @conf_type:	Type of configuration, e.g. code/data/both
  * @res:	The rdt_resource for this entry
  */
 struct resctrl_schema {
