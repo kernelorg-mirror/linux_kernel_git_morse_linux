15dd188c59ef9e6ef8755e3b95967d47a287a31d
James Morse <james.morse@arm.com>
2019-03-19 14:34:42 +0000

    x86/resctrl: Add domain offline callback for resctrl work
    When the last cpu in a domain is offline, the domain is freed. Something
    needs to remove mondata subdirectories and cancel the mbm/limbo pending
    work. Today, this is done by the arch code.
    
    Split this out into a new resctrl_offline_domain().
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- include/linux/resctrl.h
+++ include/linux/resctrl.h
@@ -204,5 +204,6 @@ struct resctrl_schema {
 };
 
 void resctrl_online_domain(struct rdt_resource *r, struct rdt_domain *d);
+void resctrl_offline_domain(struct rdt_resource *r, struct rdt_domain *d);
 
 #endif /* _RESCTRL_H */
