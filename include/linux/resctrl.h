088abcb57fdf4be6b52e4caa66f6eba10ec85b43
James Morse <james.morse@arm.com>
2019-03-19 12:52:42 +0000

    x86/resctrl: Create mba_sc configuration in the rdt_domain
    The MBA software controller expects the arch code to store its
    mbps_val which it reads back when modifying the ctrl val.
    As the MBA software controller is all-software, it shouldn't need
    any support from the arch code, which should only concerned with
    the arch specific hardware.
    
    Start pulling the mba_sc values out of hw structs.
    
    Add a struct resctrl_mba_sc array to the struct rdt_domain,
    and initialise its mbps_val to the default.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- include/linux/resctrl.h
+++ include/linux/resctrl.h
@@ -20,6 +20,9 @@ int proc_resctrl_show(struct seq_file *m,
 /* Closids are stored as a bitmap in a u32 */
 #define RESCTRL_MAX_CLOSID 32
 
+/* max value for struct resctrl_mba_sc's mbps_val */
+#define MBA_MAX_MBPS   U32_MAX
+
 /*
  * The longest name we expect in the schemata file:
  */
@@ -48,6 +51,14 @@ struct resctrl_staged_config {
 };
 
 /**
+ * struct resctrl_mba_sc - per-closid values for the control loop
+ * @mbps_val:		The user's specified control value
+ */
+struct resctrl_mba_sc {
+	u32		mbps_val;
+};
+
+/**
  * struct rdt_domain - group of cpus sharing an RDT resource
  * @list:		all instances of this resource
  * @id:			unique id for this instance
@@ -61,6 +72,7 @@ struct resctrl_staged_config {
  * @cqm_work_cpu:	worker cpu for CQM h/w counters
  * @plr:		pseudo-locked region (if any) associated with domain
  * @staged_config:	parsed configuration to be applied
+ * @mba_sc:	the mba software controller properties, indexed by closid
  */
 struct rdt_domain {
 	struct list_head		list;
@@ -77,6 +89,7 @@ struct rdt_domain {
 
 	struct pseudo_lock_region	*plr;
 	struct resctrl_staged_config	staged_config[NUM_CDP_TYPES];
+	struct resctrl_mba_sc		*mba_sc;
 };
 
 /**
