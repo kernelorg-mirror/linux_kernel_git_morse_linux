f0885577d23dc2154360b53be6f698036a84ed9f
James Morse <james.morse@arm.com>
2019-03-19 15:06:49 +0000

    x86/resctrl: Remove set_mba_sc()s control array re-initialisation
    set_mba_sc() is called by rdt_enable_ctx() during mount()
    and rdt_kill_sb(). It currently re-initialises the arch code's control
    value array.
    
    These values are already set to their default when the domain is created,
    and when rdt_kill_sb() is called, (via reset_all_ctrls()). set_mba_sc()s
    extra call to setup_default_ctrlval() isn't needed as the values are
    already at their defaults due to the creation of the domain, or reset
    during umount().
    
    Remove it.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- arch/x86/kernel/cpu/resctrl/rdtgroup.c
+++ arch/x86/kernel/cpu/resctrl/rdtgroup.c
@@ -1956,18 +1956,12 @@ static void mba_sc_destroy(struct rdt_resource *r)
 static int set_mba_sc(bool mba_sc)
 {
 	struct rdt_resource *r = &rdt_resources_all[RDT_RESOURCE_MBA].r_resctrl;
-	struct rdt_hw_domain *hw_dom;
-	struct rdt_domain *d;
 
 	if (!is_mbm_enabled() || !is_mba_linear() ||
 	    mba_sc == is_mba_sc(r))
 		return -EINVAL;
 
 	r->membw.mba_sc = mba_sc;
-	list_for_each_entry(d, &r->domains, list) {
-		hw_dom = resctrl_to_arch_dom(d);
-		setup_default_ctrlval(r, hw_dom->ctrl_val);
-	}
 
 	if (is_mba_sc(r))
 		return mba_sc_allocate(r);
