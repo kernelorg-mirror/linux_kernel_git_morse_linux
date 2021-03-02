31590b76d39b7743d3f00e0bc23534b2eab01794
James Morse <james.morse@arm.com>
2021-03-02 13:27:37 +0000

    x86/resctrl: Allow different CODE/DATA configurations to be staged
    Now that the staged configuration is grouped as a singel struct, convert
    it to an array to allow resctrl to stage more than one configuration at
    a time for a single resource and closid.
    
    To detect that the same schema is being specified twice when the schemata
    file is written, the same slot in the staged_configuration array must be
    used for each schema. Use the conf_type enum directly as an index.
    
    Reviewed-by: Jamie Iles <jamie@nuviainc.com>
    Signed-off-by: James Morse <james.morse@arm.com>
    ---
    Changes since v1:
     * Renamed max enum value CDP_NUM_TYPES
     * Whitespace and parenthesis
     * Missing word in the commit message

--- include/linux/resctrl.h
+++ include/linux/resctrl.h
@@ -21,6 +21,9 @@ enum resctrl_conf_type {
 	CDP_DATA,
 };
 
+#define CDP_NUM_TYPES	(CDP_DATA + 1)
+
+
 /**
  * struct resctrl_staged_config - parsed configuration to be applied
  * @new_ctrl:		new ctrl value to be loaded
@@ -58,7 +61,7 @@ struct rdt_domain {
 	int				mbm_work_cpu;
 	int				cqm_work_cpu;
 	struct pseudo_lock_region	*plr;
-	struct resctrl_staged_config	staged_config;
+	struct resctrl_staged_config	staged_config[CDP_NUM_TYPES];
 };
 
 /**
