d69569a733100394b3e22e7fcef6f5013827ac3b
James Morse <james.morse@arm.com>
2021-04-19 11:50:19 +0100

    arm64: irq: Collect GIC_IRQ_TYPE definitions into one place
    To pass a GIC_IRQ_TYPE_ value via ACPI's fwspec in acpi_register_gsi(),
    the value needs to be defined in a header that exists for ia64 and x86.
    
    Instead of adding a third place that GIC_IRQ_TYPE_s are defined, move
    the two existing ones to asm/irq.h.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- arch/arm64/include/asm/irq.h
+++ arch/arm64/include/asm/irq.h
@@ -6,6 +6,13 @@
 
 #include <asm-generic/irq.h>
 
+/*
+ * We need a value to serve as a irq-type for LPIs. Choose one that will
+ * hopefully pique the interest of the reviewer.
+ */
+#define GIC_IRQ_TYPE_LPI		0xa110c8ed
+#define GIC_IRQ_TYPE_PARTITION		(GIC_IRQ_TYPE_LPI + 1)
+
 struct pt_regs;
 
 int set_handle_irq(void (*handle_irq)(struct pt_regs *));
