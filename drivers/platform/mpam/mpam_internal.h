1b3a991e943eaecf996ddd062313f0e6b57ddec5
James Morse <james.morse@arm.com>
2023-03-09 14:01:42 +0000

    untested: arm_mpam: resctrl: Allow monitors to be configured
    MPAM MSCs may have support for filtering reads or writes when monitoring
    traffic. Resctrl has a configuration bitmap for which kind of accesses
    should be monitored.
    
    Bridge the gap where possible. MPAM only has a read/write bit, so not
    all the combinations can be supported.
    
    CC: Rohit Mathew <Rohit.Mathew@arm.com>
    Signed-off-by: James Morse <james.morse@arm.com>

--- drivers/platform/mpam/mpam_internal.h
+++ drivers/platform/mpam/mpam_internal.h
@@ -18,6 +18,12 @@
 /* Value to indicate the allocated monitor is derived from the RMID index. */
 #define USE_RMID_IDX	(U16_MAX + 1)
 
+/*
+ * Only these event configuration bits are supported. MPAM can't know if
+ * data is being written back, these will show up as a write.
+ */
+#define MPAM_RESTRL_EVT_CONFIG_VALID	(READS_TO_LOCAL_MEM | NON_TEMP_WRITE_TO_LOCAL_MEM)
+
 static inline bool mpam_is_enabled(void)
 {
 	return static_branch_likely(&mpam_enabled);
@@ -238,6 +244,8 @@ struct mpam_msc_ris {
 struct mpam_resctrl_dom {
 	struct mpam_component	*comp;
 	struct rdt_domain	resctrl_dom;
+
+	u32			mbm_local_evt_cfg;
 };
 
 struct mpam_resctrl_res {
@@ -297,6 +305,7 @@ int mpam_apply_config(struct mpam_component *comp, u16 partid,
 int mpam_msmon_read(struct mpam_component *comp, struct mon_cfg *ctx,
 		    enum mpam_device_features, u64 *val);
 void mpam_msmon_reset_mbwu(struct mpam_component *comp, struct mon_cfg *ctx);
+void mpam_msmon_reset_all_mbwu(struct mpam_component *comp);
 
 int mpam_resctrl_online_cpu(unsigned int cpu);
 int mpam_resctrl_offline_cpu(unsigned int cpu);
