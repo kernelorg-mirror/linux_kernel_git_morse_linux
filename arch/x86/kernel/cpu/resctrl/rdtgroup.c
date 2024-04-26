05566d21bb04b66e620e919619364c5f8211c1e5
James Morse <james.morse@arm.com>
2024-04-26 16:08:57 +0100

    x86/resctrl: Drop __init/__exit on assorted symbols
    Because ARM's MPAM controls are probed using MMIO, resctrl can't be
    initialised until enough CPUs are online to have determined the
    system-wide supported num_closid. Arm64 also supports 'late onlined
    secondaries', where only a subset of CPUs are online during boot.
    
    These two combine to mean the MPAM driver may not be able to initialise
    resctrl until user-space has brought 'enough' CPUs online.
    
    To allow MPAM to initialise resctrl after __init text has been free'd,
    remove all the __init markings from resctrl.
    
    The existing __exit markings cause these functions to be removed by the
    linker as it has never been possible to build resctrl as a module. MPAM
    has an error interrupt which causes the driver to reset and disable
    itself. Remove the __exit markings to allow the MPAM driver to tear down
    resctrl when an error occurs.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Tested-by: Carl Worth <carl@os.amperecomputing.com> # arm64
    Tested-by: Shaopeng Tan <tan.shaopeng@jp.fujitsu.com>
    Reviewed-by: Shaopeng Tan <tan.shaopeng@jp.fujitsu.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    ---
    Changes since v4:
     * Earlier __init marker removal migrated here.

--- arch/x86/kernel/cpu/resctrl/rdtgroup.c
+++ arch/x86/kernel/cpu/resctrl/rdtgroup.c
@@ -2064,7 +2064,7 @@ static struct rftype *rdtgroup_get_rftype_by_name(const char *name)
 	return NULL;
 }
 
-static void __init thread_throttle_mode_init(void)
+static void thread_throttle_mode_init(void)
 {
 	enum membw_throttle_mode throttle_mode = THREAD_THROTTLE_UNDEFINED;
 	struct rdt_resource *r_mba, *r_smba;
@@ -2090,7 +2090,7 @@ static void __init thread_throttle_mode_init(void)
 	rft->fflags = RFTYPE_CTRL_INFO | RFTYPE_RES_MB;
 }
 
-void __init mbm_config_rftype_init(const char *config)
+void mbm_config_rftype_init(const char *config)
 {
 	struct rftype *rft;
 
@@ -4078,7 +4078,7 @@ static void rdtgroup_destroy_root(void)
 	}
 }
 
-static void __init rdtgroup_setup_default(void)
+static void rdtgroup_setup_default(void)
 {
 	mutex_lock(&rdtgroup_mutex);
 
@@ -4294,7 +4294,7 @@ out_unlock:
  *
  * Return: 0 on success or -errno
  */
-int __init resctrl_init(void)
+int resctrl_init(void)
 {
 	int ret = 0;
 
@@ -4351,7 +4351,7 @@ cleanup_mountpoint:
 	return ret;
 }
 
-void __exit resctrl_exit(void)
+void resctrl_exit(void)
 {
 	mutex_lock(&rdtgroup_mutex);
 	rdtgroup_destroy_root();
