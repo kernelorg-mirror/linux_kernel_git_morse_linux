32f1b3307092343140bb5d1f7918eb28cf7b99ec
James Morse <james.morse@arm.com>
2024-04-26 16:08:35 +0100

    x86/resctrl: Export resctrl fs's init function
    rdtgroup_init() needs exporting so that arch code can call it once
    it lives in core code. As this is one of the few functions exported,
    rename it to have "resctrl" in the name. The same goes for the exit
    call.
    
    x86's arch code init functions for RDT are renamed to have an arch
    prefix to make it clear these are part of the architecture code.
    
    Co-developed-by: Dave Martin <Dave.Martin@arm.com>
    Signed-off-by: Dave Martin <Dave.Martin@arm.com>
    Signed-off-by: James Morse <james.morse@arm.com>
    Tested-by: Carl Worth <carl@os.amperecomputing.com> # arm64
    Reviewed-by: Shaopeng Tan <tan.shaopeng@jp.fujitsu.com>
    ---
    Changes since v1:
     * Rename stale rdtgroup_init() to resctrl_init() in
       arch/x86/kernel/cpu/resctrl/monitor.c comments.
    
       No functional change.
    
     * [Commit message only] Minor rewording to avoid "impersonating code".
    
     * [Commit message only] Typo fix:
       s/to have the resctrl/to have resctrl/ in commit message.

--- include/linux/resctrl.h
+++ include/linux/resctrl.h
@@ -426,4 +426,7 @@ u32 resctrl_arch_event_config_get(struct rdt_mon_domain *d,
 extern unsigned int resctrl_rmid_realloc_threshold;
 extern unsigned int resctrl_rmid_realloc_limit;
 
+int __init resctrl_init(void);
+void __exit resctrl_exit(void);
+
 #endif /* _RESCTRL_H */
