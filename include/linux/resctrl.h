32cedbd46c73f467c4a2c4cf9dd3f51be49e0422
James Morse <james.morse@arm.com>
2024-07-01 15:50:51 +0100

    x86/resctrl: Use schema type to determine how to parse schema values
    Resctrl's architecture code gets to specify a function pointer that is
    used when parsing schema entries. This is expected to be one of two
    helpers from the filesystem code.
    
    Setting this function pointer allows the architecture code to change
    the ABI resctrl presents to user-space, and forces resctrl to expose
    these helpers.
    
    Instead, add a schema format enum to choose which schema parser to
    use. This allows the helpers to be made static and the structs used
    for passing arguments moved out of shared headers.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Tested-by: Carl Worth <carl@os.amperecomputing.com> # arm64
    ---
    Changes since v4:
     * Creation of the enum moves into this patch - review tags not picked up.
     * Removed some whitespace.
    
    Changes since v3:
     * Removed a spurious semicolon
    
    Changes since v2:
     * This patch is new

--- include/linux/resctrl.h
+++ include/linux/resctrl.h
@@ -185,7 +185,6 @@ struct resctrl_membw {
 	u32				*mb_map;
 };
 
-struct rdt_parse_data;
 struct resctrl_schema;
 
 enum resctrl_scope {
@@ -208,6 +207,18 @@ struct resctrl_mon {
 	struct list_head	evt_list;
 };
 
+/*
+ * enum resctrl_schema_fmt - The format user-space provides for a schema.
+ * @RESCTRL_SCHEMA_BITMAP:	The schema is a bitmap in hex.
+ * @RESCTRL_SCHEMA_PERCENTAGE:	The schema is a decimal percentage value.
+ * @RESCTRL_SCHEMA_MBPS:	The schema is a decimal MBps value.
+ */
+enum resctrl_schema_fmt {
+	RESCTRL_SCHEMA_BITMAP,
+	RESCTRL_SCHEMA_PERCENTAGE,
+	RESCTRL_SCHEMA_MBPS,
+};
+
 /**
  * struct rdt_resource - attributes of a resctrl resource
  * @rid:		The index of the resource
@@ -223,7 +234,7 @@ struct resctrl_mon {
  * @data_width:		Character width of data when displaying
  * @default_ctrl:	Specifies default cache cbm or memory B/W percent.
  * @format_str:		Per resource format string to show domain value
- * @parse_ctrlval:	Per resource function pointer to parse control values
+ * @schema_fmt:	Which format string and parser is used for this schema.
  * @evt_list:		List of monitoring events
  * @cdp_capable:	Is the CDP feature available on this resource
  */
@@ -242,9 +253,7 @@ struct rdt_resource {
 	int			data_width;
 	u32			default_ctrl;
 	const char		*format_str;
-	int			(*parse_ctrlval)(struct rdt_parse_data *data,
-						 struct resctrl_schema *s,
-						 struct rdt_ctrl_domain *d);
+	enum resctrl_schema_fmt	schema_fmt;
 	struct list_head	evt_list;
 	bool			cdp_capable;
 };
