15b0ce401fd2a09f0111ace17cc611f84c6a4562
James Morse <james.morse@arm.com>
2025-01-21 13:52:36 +0000

    arm64: proton-pack: Disable Spectre-BSE mitigation by default
    The Spectre-BSE vulnerability is hard to exploit, requiring abuse of
    the branch history (which is small on these cores), confusing the branch
    predictor, and finding a useful gadget to expose data.
    
    Depending on the frequency of syscalls, the cost of mitigating this
    is high - and platforms cannot chose between Spectre-v2 mitigation and
    Spectre-BSE mitigation as they use the same firmware call.
    
    Disable the Spectre-BSE mitigation by default, and add a command line
    option to enable it in environments where this vulnerability is
    considered a problem.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- arch/arm64/kernel/proton-pack.c
+++ arch/arm64/kernel/proton-pack.c
@@ -1158,6 +1158,15 @@ static void kvm_setup_bhb_slot(const char *hyp_vecs_start)
 static void kvm_setup_bhb_slot(const char *hyp_vecs_start) { }
 #endif /* CONFIG_KVM */
 
+/* Spectre-BSE mitigations are disabled by default */
+static bool __read_mostly __spectre_bse;
+static int __init parse_spectre_bse_param(char *str)
+{
+	__spectre_bse = true;
+	return 0;
+}
+early_param("spectre_bse", parse_spectre_bse_param);
+
 static void spectre_bhb_enable_fw_mitigation(enum bhb_mitigation_bits fw_wa)
 {
 	char *chosen_slot = __smccc_workaround_3_smc;
@@ -1244,7 +1253,7 @@ void spectre_bhb_enable_mitigation(const struct arm64_cpu_capabilities *entry)
 	/* Spectre BSE needs to upgrade the BHB mitigation to use firmware */
 	if (bse_upgrade_loop_mitigation) {
 		bse_state = spectre_bse_get_cpu_fw_mitigation_state(BHB_FW_WA1);
-		if (bse_state == SPECTRE_MITIGATED) {
+		if (bse_state == SPECTRE_MITIGATED && __spectre_bse) {
 			/*
 			 * For affected cores the firmware implementions of WA1
 			 * and WA3 are both sufficient for BSE, but what about
